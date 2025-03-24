e81f1eba90fc6ab59ca0eab74283abe9e0ac0718
Raenon <krstfboda@gmail.com>
2025-03-24 23:08:22 +0100

    create users table habár nem tudom miért

--- database/migrations/2025_01_25_154830_create_users_table.php
+++ database/migrations/2025_01_25_154830_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('username');
             $table->string('email')->unique();
             $table->string('password');
-            $table->number('userType');
+            $table->integer('userType');
             $table->timestamps();
         });
     }
